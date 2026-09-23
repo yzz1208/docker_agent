cfb88cadd6e9a510acee1c14c07914832c226da8
yzz1208 <94884597+yzz1208@users.noreply.github.com>
2026-09-23 10:51:09 +0800

    feat: export orchestration delegation primitives

---
+++ src/docker_agent/orchestration/__init__.py
@@ -0,0 +1,23 @@
+from docker_agent.orchestration.delegation import (
+    AgentCapabilityIndex,
+    AgentHandoff,
+    DelegationCapabilityUnavailable,
+    DelegationContext,
+    DelegationHopLimitExceeded,
+    DelegationLoopDetected,
+    DelegationPolicy,
+    DelegationPolicyError,
+    DelegationSourceMismatch,
+)
+
+__all__ = [
+    "AgentCapabilityIndex",
+    "AgentHandoff",
+    "DelegationCapabilityUnavailable",
+    "DelegationContext",
+    "DelegationHopLimitExceeded",
+    "DelegationLoopDetected",
+    "DelegationPolicy",
+    "DelegationPolicyError",
+    "DelegationSourceMismatch",
+]
